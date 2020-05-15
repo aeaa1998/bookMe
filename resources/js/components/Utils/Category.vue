86d2d0c10c64890c8246de24dc5c09223148ada6
Douglas de León <douglasdeleon@Douglass-MacBook-Pro.local>
2020-05-15 13:17:12 -0600

    Book view

---
+++ resources/js/components/Utils/Category.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+
+        <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+            <div v-bind:key="book.id" v-for="book in books">
+                <li>
+                    <BookCard v-bind:book="book"/>
+                </li>
+            </div>
+
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+    </div>
+</template>
+
+<script>
+import BookCard from './BookCard.vue';
+
+export default {
+    name: 'Category',
+    components: {
+        BookCard,
+    },
+    props: ['books'],
+}
+</script>
--- resources/js/components/dashboard/Books/Rent.vue
+++ resources/js/components/dashboard/Books/Rent.vue
@@ -9,31 +9,24 @@
         uk-grid
       >
         <div v-for="i in [1,2,3,4,5,6,7,8,9,10]" v-bind:key="i">
-          <div class="uk-card uk-card-default">
-            <div
-              uk-lightbox
-              class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-position-relative"
-              data-src="https://www.bookcoversclub.com/wp-content/uploads/2017/07/book-cover-338.jpg"
-              uk-img
-              alt
-            >
-              <div class="uk-card-media-top">
-                <a
-                  class="uk-button uk-button-text uk-position-absolute uk-position-bottom-right uk-position-small"
-                  href="https://www.bookcoversclub.com/wp-content/uploads/2017/07/book-cover-338.jpg"
-                  data-caption="Titulo del libro"
-                >Agrandar</a>
-              </div>
-            </div>
-
-            <div class="uk-padding-small">
-              <div class="uk-card-title">Titulo del libro</div>
-              <div>Autor, Edicion</div>
-              <div>Precio Venta: 500Q</div>
-            </div>
-          </div>
+          <BookCard />
         </div>
       </div>
     </div>
+    <BookModal />
   </div>
 </template>
+
+<script>
+import BookCard from '../../Utils/BookCard.vue';
+import BookModal from '../../Utils/BookModal.vue';
+
+export default {
+  name: "Rent",
+  components:{
+    BookCard,
+    BookModal,
+  },
+  props: ['books']
+}
+</script>
